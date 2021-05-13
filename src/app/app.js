fad0893a88f4d194e7a555e7127140761b230234
Joseph nannepaga <joseph.nannepaga@gmail.com>
2021-05-13 12:41:23 +0530

    Shifted GIT repos

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component{
+    componentDidMount(){
+        console.log('Say Like moounit');
+    }
+    render(){
+        return(<h1>Welcome</h1>)
+    }    
+}
+
+export default App;
